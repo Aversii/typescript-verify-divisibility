bd291cac23208ce6c3aebe8b5bb9564009e623dd
aversii <lucasaversi@gmail.com>
2023-10-19 19:44:37 -0300

    feat:  Divisibility by 2 and Divisibility by 3 implemented

--- index.js
+++ index.js
@@ -9,8 +9,29 @@ let greetings = () => {
     console.log("hello, this is a program which shows a divisibility of a picked number");
 };
 let getNumberAndConvertToArray = () => {
-    const result = prompt("Pick a number ");
+    const result = prompt("Pick a number  ");
     return result === null || result === void 0 ? void 0 : result.split("").map(item => Number(item));
 };
+let veriFyDivisibilityBy2 = (arrayOfAbsolut) => {
+    if (arrayOfAbsolut && arrayOfAbsolut[arrayOfAbsolut.length - 1] % 2 === 0) {
+        console.log("é divisivel por 2");
+    }
+    else {
+        console.log("não é divisivel por 2");
+    }
+};
+let veriFyDivisibilityBy3 = (arrayOfAbsolut) => {
+    if (arrayOfAbsolut && arrayOfAbsolut.reduce((acc, value) => acc + value, 0) % 3 === 0) {
+        console.log("é divisivel por 3");
+    }
+    else {
+        console.log("não é divisivel por 3");
+    }
+};
 greetings();
-console.log(getNumberAndConvertToArray());
+let verifyAll = () => {
+    let arrayOfAbsolut = getNumberAndConvertToArray();
+    veriFyDivisibilityBy2(arrayOfAbsolut);
+    veriFyDivisibilityBy3(arrayOfAbsolut);
+};
+verifyAll();
--- index.ts
+++ index.ts
@@ -7,9 +7,33 @@ let greetings = ()=>{
 }
 
 let getNumberAndConvertToArray=  ():number[] | null|undefined => {
-    const result = prompt("Pick a number ")
+    const result = prompt("Pick a number  ")
     return result?.split("").map(item=>Number(item))
 }
 
+
+
+let veriFyDivisibilityBy2 = (arrayOfAbsolut:any)=>{
+    if(arrayOfAbsolut && arrayOfAbsolut[arrayOfAbsolut.length-1]%2===0){
+        console.log("é divisivel por 2")
+    }else{    console.log("não é divisivel por 2")
+    }
+}
+
+let veriFyDivisibilityBy3 = (arrayOfAbsolut:any)=>{
+    if(arrayOfAbsolut && arrayOfAbsolut.reduce((acc:any, value:any)=>acc+value,0)%3===0){
+        console.log("é divisivel por 3")
+    }else{    console.log("não é divisivel por 3")
+    }
+}
+
+
 greetings()
-console.log(getNumberAndConvertToArray())
+
+let verifyAll= () =>{
+    let arrayOfAbsolut = getNumberAndConvertToArray()
+    veriFyDivisibilityBy2(arrayOfAbsolut)
+    veriFyDivisibilityBy3(arrayOfAbsolut)
+}
+
+verifyAll()
